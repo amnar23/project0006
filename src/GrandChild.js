ec9aefad0e402dea1931f17215f0b2075a42824a
Amna <amnar23498@gmail.com>
2020-09-02 00:15:45 +0500

    useReducer
    Increment/Decrement using Reducer

--- src/GrandChild.js
+++ src/GrandChild.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, {useReducer} from 'react';
 import ValueContext from './ValueContext'
 import { useContext } from 'react';
+import NumberReducer from './NumberReducer';
 
 function GrandChild() {
     let value=useContext(ValueContext);
     let num=value[0];
-    let [number,setNumber]=useState(num);
+    let [number,dispatch]=useReducer(NumberReducer,num);
   return (
-    <div>I am GrandChild - The value is {number} - I will change my own value only<br/>
-    Let's Incerement <button onClick={()=>setNumber(++number)} >Click Here</button><br/>
-    Let's Decrement <button onClick={()=>setNumber(--number)} >Click Here</button></div>
+    <div>I am GrandChild - The value is {number} - I will change my own value only using 'useReducer'<br/>
+    Let's Incerement <button onClick={()=>dispatch({type:"INCREMENT"})} >Click Here</button><br/>
+    Let's Decrement <button onClick={()=>dispatch({type:'DECREMENT'})} >Click Here</button></div>
 
   );
 }
